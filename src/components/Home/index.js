05390df0d0345c4cffb3bb2c00aa1367680c1102
Angel Torres <103345941+angeljtm@users.noreply.github.com>
2023-11-15 20:44:44 -0800

    Started with home layout

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                <img src={LogoTitle} alt="developer" />
+                ngel
+                <br />
+                CS Student
+                </h1>
+                <h2>CS Student / Future Web Developer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+
+export default Home 
