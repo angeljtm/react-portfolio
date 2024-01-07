a9409348d22c4c0d3c469eb4491d7bb4ba8007bb
Angel Torres <103345941+angeljtm@users.noreply.github.com>
2024-01-06 16:33:21 -0800

    working on Logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -36,6 +37,7 @@ const Home = () => {
                 <h2>CS Student / Future Web Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     );
 }
