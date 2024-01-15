b25043054bd4ea465a72b4b4a5d54d8578afdc6d
Angel Torres <103345941+angeljtm@users.noreply.github.com>
2024-01-15 14:06:33 -0800

    Created About me title and body

---
+++ src/components/About/index.js
@@ -0,0 +1,66 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+
+const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    },[])
+
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['A','b','o','u','t',' ','m','e']}
+                    idx={15}/>
+                    
+                </h1>
+                <p>
+                Hello! I'm Angel Torres, a passionate 19-year-old currently immersed in the world of computer science.
+                </p>
+                <p>
+                I'm pursuing my major at Fresno City College with big plans to transfer soon and continue my academic journey. My coding journey has led me through classes in C++ and Java, and I've got a solid grasp of HTML and CSS.
+                </p>
+                <p> 
+                Beyond the screen, I find joy in capturing moments through film photography and maintaining a healthy lifestyle by indulging in my love for running. Life's an exciting blend of code, creativity, and the great outdoors for me!
+                </p>
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faCss3} color="#DD0031"/>
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faHtml5} color="#DD0031"/>
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faGitAlt} color="#DD0031"/>
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faReact} color="#DD0031"/>
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faJsSquare} color="#DD0031"/>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default About
