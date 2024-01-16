fdcb26ad77a2f5d8620396aabbae28c51dbbb3c8
Angel Torres <103345941+angeljtm@users.noreply.github.com>
2024-01-15 18:15:21 -0800

    created loading animation between pages

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo'
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -17,6 +18,7 @@ const Home = () => {
     },[])
 
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -39,6 +41,8 @@ const Home = () => {
             </div>
             <Logo />
         </div>
+        <Loader type="ball-rotate" />
+        </>
     );
 }
 
