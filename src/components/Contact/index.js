1246acd5636bf76b294d3942f1a9c956b97d2a78
Angel Torres <103345941+angeljtm@users.noreply.github.com>
2024-01-16 00:26:25 -0800

    completed contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import Loader from 'react-loaders'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 3000)
     },[])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+        .sendForm(
+            'service_0h2fzyi',
+            'template_4fy2nqw',
+            form.current,
+            'xQE29wgShS59sE_s6'
+        )
+        .then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again.')
+            }
+        )
+    }
     
     return (
         <>
@@ -27,13 +51,13 @@ const Contact = () => {
                     I am interested in internship oppurtunities revolving around web development. However, if you have any other requests or questions, don't hesitate to contact me using the form below.
                 </p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={form} on onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
-                                <input type='text' name='name' placeholder='Name' required/>
+                                <input placeholder='Name' type='text' name='name' required/>
                             </li>
                             <li className='half'>
-                                <input type='email' name='email' placeholder='Email' required/>
+                                <input placeholder='Email' type='email' name='email' required/>
                             </li>
                             <li>
                                 <input placeholder="Subject" type="text" name='subject' required/>
@@ -47,6 +71,23 @@ const Contact = () => {
                         </ul>
                     </form>
                 </div>
+            </div>
+            <div className='info-map'>
+                Angel Torres
+                <br/>
+                Fresno, California
+                <br/>
+                93721
+                <br/>
+                <span>anjeltor@icloud.com</span>
+            </div>
+            <div className='map-wrap'>
+                <MapContainer center={[36.7378, -119.7871]} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    <Marker position={[36.7378, -119.7871]}>
+                        <Popup>Angel lives here, come on over for a cup of coffe :)</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
         </div>
         <Loader type='ball-rotate' />
